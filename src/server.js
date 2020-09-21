33525dc6625acce5edff2aad8b12ba1a278cdcd1
BenGoldfeder <goldfedb@gmail.com>
2020-09-20 20:47:03 -0400

    converted example to API

--- src/server.js
+++ src/server.js
@@ -1,20 +1,36 @@
 const http = require('http');
 const url = require('url');
 const query = require('querystring');
+
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
+
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-  '/': htmlHandler.getIndex,
-  '/success': jsonHandler.success,
-  '/badRequest': jsonHandler.badRequest,
-  notFound: jsonHandler.notFound,
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getStyle,
+    '/success': jsonHandler.success,
+    '/badRequest': jsonHandler.badRequest,
+    '/unauthorized': jsonHandler.unauthorized,
+    '/forbidden': jsonHandler.forbidden,
+    '/internal': jsonHandler.internal,
+    '/notImplemented': jsonHandler.notImplemented,
+    notFound: jsonHandler.notFound,
+    index: htmlHandler.getIndex
 };
 
 const onRequest = (request, response) => {
-
+    const parsedURL = url.parse(request.url);
+    const acceptedTypes = request.headers.accept.split(',');
+    const params = query.parse(parsedURL.query);
+    
+    if(urlStruct[parsedURL.pathname]) {
+        urlStruct[parsedURL.pathname](request, response, params);
+    } else {
+        urlStruct.notFound(request, response);
+    }
 };
 
 http.createServer(onRequest).listen(port);
